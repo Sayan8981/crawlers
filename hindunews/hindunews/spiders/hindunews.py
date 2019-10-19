123442e14405cf40c4845a5c62eb4052e1651cf6
saayan <saayan8981@gmail.com>
2019-10-19 14:14:29 +0530

    my first commit#1

--- hindunews/hindunews/spiders/hindunews.py
+++ hindunews/hindunews/spiders/hindunews.py
@@ -7,4 +7,15 @@ class hindunews(scrapy.Spider):
 
     def parse(self,response):
         #import pdb;pdb.set_trace()
-        print (response.body)
+        #print (response.body)
+        sel=scrapy.Selector(response)
+        url=sel.xpath('//div[@class="col-xs-12"]/nav/div[@id="main-menu"]/ul/li[@class="dropdown"]/a/@href').extract()
+        print(url)
+        for url in url:
+        	if "news" in url:
+        		news_url=url
+        yield scrapy.Request(news_url, callback=self.parse_url,dont_filter=True)
+
+    def parse_url(self,news_url):
+        print (news_url.url)
+        print (news_url.body)    
