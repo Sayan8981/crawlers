966c01a374025e0bb976028e6e86926efe5887d8
Caavo <saayan8981@gmail.com>
2019-10-21 12:01:46 +0530

    first commit#9

--- hindunews/hindunews/pipelines.py
+++ hindunews/hindunews/pipelines.py
@@ -20,8 +20,16 @@ class HindunewsPipeline(object):
         passwd=input(str)
         self.connection=MySQLdb.connect(host="localhost",user=user,passwd=passwd,db="Hindunews")
         self.cursor=self.connection.cursor() 
+        self.counter=0
 
     def process_item(self, item, spider):
         if item['section']=='National':
             print (item)
+            self.query="insert into National_news_details (sk_key,News_headlines,News_intro,News_details,Country,Date,Updated_at,News_url) values (%s,%s,%s,%s,%s,%s,%s,%s)"
+            self.cursor.execute(self.query,(item['sk_key'],item['news_headlines'],item['news_tagline'],item['news_details'],
+                                          item['country'],item['date'],item['updated_at'],item['news_url']))
+            self.counter+=1
+            self.connection.autocommit(True)
+            print("\n")
+            print ("Total commit: ", self.counter)
             return item
