72e8f078626851f98119ec849d88116c552e3feb
Caavo <saayan8981@gmail.com>
2019-10-22 11:56:08 +0530

    first commit #15

--- hindunews/hindunews/spiders/hindunews_browse.py
+++ hindunews/hindunews/spiders/hindunews_browse.py
@@ -79,7 +79,7 @@ class hindunews(Spider):
         national_news_item['section']='National'
         national_news_item['news_headlines']=sel.xpath(xpath.national_news_headlines_xpath).extract()
         if national_news_item['news_headlines']:
-            national_news_item['news_headlines']=national_news_item['news_headlines'][0].strip("\n ").encode('ascii','ignore')
+            national_news_item['news_headlines']=''.join(national_news_item['news_headlines']).strip("\n ").encode('ascii','ignore')
         else:
             national_news_item['news_headlines']='None'    
         national_news_item['news_tagline']=sel.xpath(xpath.national_news_tagline_xpath).extract()
@@ -88,9 +88,11 @@ class hindunews(Spider):
         else:
             national_news_item['news_tagline']='None'                   
         national_news_item['news_details']=''.join(data for data in sel.xpath(xpath.national_news_details_xpath).extract()).strip(" ").encode('ascii','ignore')
+        if national_news_item['news_details']=="":
+            national_news_item['news_details']=''.join(data for data in sel.xpath(xpath.national_news_details_alternative_xpath).extract()).strip(" ").encode('ascii','ignore')
         national_news_item['country']=sel.xpath(xpath.national_news_country_xpath).extract()[0].strip("\n,: ").encode("ascii",'ignore')
-        national_news_item['date']=sel.xpath(xpath.national_news_date_xpath).extract()[0].strip("\n ")
-        national_news_item['updated_at']=sel.xpath(xpath.national_news_updatedDate_xpath).extract()[0].strip("\n ").encode("ascii",'ignore')
+        national_news_item['date']=''.join(sel.xpath(xpath.national_news_date_xpath).extract()).strip("\n ")
+        national_news_item['updated_at']=''.join(sel.xpath(xpath.national_news_updatedDate_xpath).extract()).strip("\n ").encode("ascii",'ignore')
         national_news_item['news_url']=headlines_url.url
         national_news_item['sk_key']=hashlib.md5(headlines_url.url.encode()).hexdigest()
         yield national_news_item
