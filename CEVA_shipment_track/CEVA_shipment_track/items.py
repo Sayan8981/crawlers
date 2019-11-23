aa047303f89194f9f29e4286a9b8340248f96819
saayan <saayan8981@gmail.com>
2019-11-23 16:40:10 +0530

    #2 CEVA shipment detail extract

--- CEVA_shipment_track/CEVA_shipment_track/items.py
+++ CEVA_shipment_track/CEVA_shipment_track/items.py
@@ -11,4 +11,23 @@ import scrapy
 class CevaShipmentTrackItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    pass
+    #pass
+    waybill_number=scrapy.Field()
+    ship_date=scrapy.Field()
+    due_date=scrapy.Field()
+    estimated_delivery_date=scrapy.Field()
+    shipper_location=scrapy.Field()
+    consignee_location=scrapy.Field()
+    total_pcs=scrapy.Field()
+    actual_weight=scrapy.Field()
+    charge_weight=scrapy.Field()
+    freight_terms=scrapy.Field()
+    service_level=scrapy.Field()
+    delivery_type=scrapy.Field()
+    movement_type=scrapy.Field()
+
+    delivered=scrapy.Field()
+    out_for_delivery=scrapy.Field()
+    check_out_for_delivery=scrapy.Field()
+    pick_up=scrapy.Field()
+    booking_created=scrapy.Field()
--- CEVA_shipment_track/CEVA_shipment_track/spiders/ceva_shipment_details_extract.py
+++ CEVA_shipment_track/CEVA_shipment_track/spiders/ceva_shipment_details_extract.py
@@ -3,7 +3,7 @@ from scrapy import *
 import os
 import sys
 import hashlib
-#from CevaShipmentTrackItem.items import *
+from CEVA_shipment_track.items import *
 #import pdb;pdb.set_trace()
 print(sys.path)
 import datetime
@@ -17,11 +17,12 @@ class CEVA_shipment_track(Spider):
     start_urls=["https://etracking.cevalogistics.com/eTracking.aspx?uid="]
 
     def __init__(self):
-        self.input_array=[]
+        self.input_id=["EC90009356","EC90009210","EC90008803","EC90009014","EC90008855","EC90009003"]
+        self.shipment_details=dict()
+        self.service_info=dict()
+        self.key_event_history=dict()
      
     def parse(self,response):
-        input_id=str(input("Enter the bill number:"))
-        self.input_array.append(input_id)
         #import pdb;pdb.set_trace()
         self.viewstate=response.xpath(xpath.viewstate_xpath).extract()[0]
         self.viewstategenerator=''.join(response.xpath(xpath.viewstategenerator_xpath).extract())
@@ -42,7 +43,7 @@ class CEVA_shipment_track(Spider):
                 'Set-Cookie':cookies,
                  }
 
-        for _id in self.input_array: 
+        for _id in self.input_id: 
             data = {
                       '__EVENTTARGET': '',
                       '__EVENTARGUMENT': '',
@@ -55,13 +56,38 @@ class CEVA_shipment_track(Spider):
                       'btnSearch': 'Track'
                     }         
 
-        yield FormRequest(url=response.url,method='POST',headers=headers,formdata=data
+            yield FormRequest(url=response.url,method='POST',headers=headers,formdata=data
                                      ,callback=self.extract_data,dont_filter=True)
 
 
     def extract_data(self,response):
-        import pdb;pdb.set_trace()
-        print (response.body)    
+        #import pdb;pdb.set_trace()
+        #print (response.body) 
+        item=CevaShipmentTrackItem()
+        item["waybill_number"]=''.join(response.xpath('//td[contains(text(),"Waybill Number:")]/following-sibling::td/text()').extract())
+        item["ship_date"]=''.join(response.xpath('//td[contains(text(),"Ship Date:")]/following-sibling::td/text()').extract())
+        item["due_date"]=''.join(response.xpath('//td[contains(text(),"Due Date:")]/following-sibling::td/text()').extract())
+        item["estimated_delivery_date"]=''.join(response.xpath('//td[contains(text(),"Estimated Delivery Date:")]/following-sibling::td/text()').extract())
+        item["shipper_location"]=''.join(response.xpath('//td[contains(text(),"Shipper Location")]/following-sibling::td/text()').extract()).replace('\xa0',' ').replace("\t","").replace("\n","").replace("\r","").strip()
+        item["consignee_location"]=''.join(response.xpath('//td[contains(text(),"Consignee Location")]/following-sibling::td/text()').extract()).replace('\xa0',' ').replace("\t","").replace("\n","").replace("\r","").strip()
+
+        item["total_pcs"]=''.join(response.xpath('//td[contains(text(),"Total Pieces:")]/following-sibling::td/text()').extract()).replace('\xa0',' ').replace("\t","").replace("\n","").replace("\r","").strip()
+        item["actual_weight"]=''.join(response.xpath('//td[contains(text(),"Actual Weight:")]/following-sibling::td/text()').extract()).replace('\xa0',' ').replace("\t","").replace("\n","").replace("\r","").strip()
+        item["charge_weight"]=''.join(response.xpath('//td[contains(text(),"Charge Weight:")]/following-sibling::td/text()').extract()).replace('\xa0',' ').replace("\t","").replace("\n","").replace("\r","").strip()
+
+        item["freight_terms"]=''.join(response.xpath('//td[contains(text(),"Freight Terms:")]/following-sibling::td/text()').extract()).replace('\xa0',' ').replace("\t","").replace("\n","").replace("\r","").strip()
+        item["service_level"]=''.join(response.xpath('//td[contains(text(),"Service Level:")]/following-sibling::td/text()').extract()).replace('\xa0',' ').replace("\t","").replace("\n","").replace("\r","").strip()
+        item["delivery_type"]=''.join(response.xpath('//td[contains(text(),"Delivery Type:")]/following-sibling::td/text()').extract()).replace('\xa0',' ').replace("\t","").replace("\n","").replace("\r","").strip()
+        item["movement_type"]=''.join(response.xpath('//td[contains(text(),"Movement Type:")]/following-sibling::td/text()').extract()).replace('\xa0',' ').replace("\t","").replace("\n","").replace("\r","").strip()
+
+        item["delivered"]=','.join(response.xpath('//td[contains(text(),"Delivered")]/following-sibling::td/text()').extract()).replace('\xa0','').replace("\t",'').replace("\n",'').replace("\r",'').strip(", ").split(",")
+        item["out_for_delivery"]=','.join(response.xpath('//td[contains(text(),"Out For Delivery")]/following-sibling::td/text()').extract()).replace('\xa0','').replace("\t","").replace("\n","").replace("\r","").strip(" ").split(",")
+
+        item["pick_up"]=','.join(response.xpath('//td[contains(text(),"Pickup")]/following-sibling::td/text()').extract()).replace('\xa0','').replace("\t","").replace("\n","").replace("\r","").strip(" ").split(",")
+        item["booking_created"]=','.join(response.xpath('//td[contains(text(),"Booking Created")]/following-sibling::td/text()').extract()).replace('\xa0','').replace("\t","").replace("\n","").replace("\r","").strip(" ").split(",")
+
+        # print (self.key_event_history)
+        yield item 
 
 
 
@@ -77,10 +103,5 @@ class CEVA_shipment_track(Spider):
 
 
 """
-ceva,EC90008855
-ceva,EC90009003
-ceva,EC90009014
-ceva,EC90008803
-ceva,EC90009210
-ceva,EC90009356
+["EC90009356","EC90009210","EC90008803","EC90009014","EC90008855","EC90009003"]
 """
